574d56350bbbd4a337d2846d465de122ff70b7ec
Schrodinger ZHU Yifan <i@zhuyi.fan>
2026-01-11 07:07:16 -0500

    [frontend] add identifier mangling

--- frontend/reussir-core/src/Reussir/Core/Mangle.hs
+++ frontend/reussir-core/src/Reussir/Core/Mangle.hs
@@ -6,7 +6,12 @@ We borrow most of the ideas from rust's v0 symbol format.
 -}
 module Reussir.Core.Mangle where
 
+import Data.Char (isAscii)
+import Data.Text qualified as T
 import Data.Text.Builder.Linear as TB
+import Data.Text.Encoding qualified as TE
+import Data.Text.Punycode qualified as Punycode
+import Reussir.Parser.Types.Lexer
 
 class Manglable a where
     mangle :: a -> TB.Builder
@@ -47,3 +52,33 @@ instance Manglable B62Num where
     mangle (B62Num n)
         | n == 0 = TB.fromChar '_'
         | otherwise = encodeB62 (n - 1) <> TB.fromChar '_'
+
+{- | For path mangling, we directly use the nested path mangling
+scheme from v0 symbol format.
+-}
+instance Manglable Path where
+    mangle (Path prefix baseName) = undefined
+
+{- | For ascii identifiers, the mangle is simply length+identifier.
+| For unicode identifiers, the mangle is `u` + length + punycode'(identifier). where punycode' is the punycode encoding where `-` are replaced with `_`.
+| If the identifier (or punycode) starts with a digit, add `_` between the length and the body.
+-}
+instance Manglable Identifier where
+    mangle (Identifier name)
+        | T.all isAscii name =
+            -- ASCII: length + [_] + identifier
+            let sep = if startsWithDigit name then TB.fromChar '_' else mempty
+             in TB.fromDec (T.length name) <> sep <> TB.fromText name
+        | otherwise =
+            -- Unicode: u + length + [_] + punycode'(identifier)
+            let encoded = TE.decodeASCII $ Punycode.encode name
+                -- Replace '-' with '_' in the punycode output
+                punycode' = T.map (\c -> if c == '-' then '_' else c) encoded
+                sep = if startsWithDigit punycode' then TB.fromChar '_' else mempty
+             in TB.fromChar 'u' <> TB.fromDec (T.length punycode') <> sep <> TB.fromText punycode'
+
+-- | Check if a Text starts with a digit
+startsWithDigit :: T.Text -> Bool
+startsWithDigit t = case T.uncons t of
+    Just (c, _) -> c >= '0' && c <= '9'
+    Nothing -> False
--- frontend/reussir-core/test/Test/Reussir/Core/Mangle.hs
+++ frontend/reussir-core/test/Test/Reussir/Core/Mangle.hs
@@ -5,6 +5,7 @@ module Test.Reussir.Core.Mangle where
 import Data.Text qualified as T
 import Data.Text.Builder.Linear qualified as TB
 import Reussir.Core.Mangle
+import Reussir.Parser.Types.Lexer (Identifier (..))
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -21,8 +22,19 @@ tests =
             , testCase "63 -> 10_" $ mangleB62 63 @?= "10_"
             , testCase "1000 -> g7_" $ mangleB62 1000 @?= "g7_"
             ]
+        , testGroup
+            "Identifier encoding"
+            [ testCase "ASCII: foo -> 3foo" $ mangleIdent "foo" @?= "3foo"
+            , testCase "ASCII: bach -> 4bach" $ mangleIdent "bach" @?= "4bach"
+            , testCase "ASCII with digit: 123abc -> 6_123abc" $ mangleIdent "123abc" @?= "6_123abc"
+            , testCase "Unicode: gödel -> u8gdel_5qa" $ mangleIdent "gödel" @?= "u8gdel_5qa"
+            ]
         ]
 
 -- | Helper to mangle B62Num and extract the text
 mangleB62 :: Int -> T.Text
 mangleB62 = TB.runBuilder . mangle . B62Num
+
+-- | Helper to mangle Identifier and extract the text
+mangleIdent :: T.Text -> T.Text
+mangleIdent = TB.runBuilder . mangle . Identifier
